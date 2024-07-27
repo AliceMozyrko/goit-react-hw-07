98d97632708af052ca43cc98e5b817d4169de2fc
AliceMozyrko <mozyrkoalisa@gmail.com>
2024-07-27 11:21:19 +0300

    finish

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { PersistGate } from 'redux-persist/integration/react';
-
 import { Provider } from 'react-redux'
 import { store } from "./redux/store"
-import { persistor } from './redux/store'
 import App from "./components/App/App"
 import "modern-normalize";
 import './index.css'
@@ -12,9 +9,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <App />
-      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -12,3 +12,4 @@ const filtersSlice = createSlice({
 export default filtersSlice.reducer
 export const { changeFilter } = filtersSlice.actions
 export const selectNameFilter = state => state.filters.name
+
